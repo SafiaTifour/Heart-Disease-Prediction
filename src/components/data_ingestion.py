59d11c023508dfe5c56efd6492e7adb2454aec27
unknown <safia.tifour@ensia.edu.dz>
2024-07-31 22:06:01 +0200

    data_ingestion.py and data_transformation UPDATED

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,3 +1,52 @@
+from src.exception import CustomException
+from src.logger import logging
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import sys
+import os
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('Starting data ingestion method')
+        try:
+            logging.info("Reading the data")
+            df = pd.read_csv('src\data\heart_disease_data.csv')
+            
+            logging.info("Creating the directory for the data")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False, header= True)
+
+            logging.info("Train, test split intiated")
+
+            train_data, test_data = train_test_split(df, test_size = 0.2, random_state = 42)
+
+            logging.info("Loading the train data into the corresponding files")
+            train_data.to_csv(self.ingestion_config.train_data_path, index=False, header= True)
+            logging.info("Loading the test data into the corresponding files")
+            test_data.to_csv(self.ingestion_config.test_data_path, index=False, header= True)
+
+            logging.info("Ingestion of the data completed successfully")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path  
+                )
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
 
 
 
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,4 +1,56 @@
+import os
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+from src.components.data_ingestion import DataIngestion
+from src.components.data_ingestion import DataIngestionConfig
 
+
+class DataTransformation:
+    def __init__(self):
+        pass
+    
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Reading train and test data is completed")
+
+
+            target_column_name = "target"
+
+            input_train = train_df.drop(columns = [target_column_name], axis = 1)
+            target_train = train_df[target_column_name]
+
+            input_test = test_df.drop(columns = [target_column_name], axis = 1)
+            target_test = test_df[target_column_name]
+
+            train_arr_x = np.array(input_train)
+            test_arr_x = np.array(input_test)
+
+            train_arr_y = np.array(target_train)
+            test_arr_y = np.array(target_test)
+
+            return (
+                train_arr_x,
+                test_arr_x,
+                train_arr_y,
+                test_arr_y,
+            )
+
+            
+
+        except Exception as e:
+            raise CustomException(e, sys)
 
 
 
